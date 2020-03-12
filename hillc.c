f9ff14d6ae0aabe14ff56612cd6e11ff6d55ed27
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2020-03-12 11:49:38 +0530

    Add files via upload

---
+++ hillc.c
@@ -0,0 +1,134 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+float encrypt[3][1], decrypt[3][1], a[3][3], b[3][3], mes[3][1], c[3][3];
+
+void encryption();    //encrypts the message
+void decryption();    //decrypts the message
+void getKeyMessage();    //gets key and message from user
+void inverse();        //finds inverse of key matrix
+
+int main() {
+ int choice;
+ system("cls");
+ while(1)
+ {
+  printf("Enter Your choice:\n1.Encrypt\t2.Decrypt\t3.Exit\n");
+  scanf("%d",&choice);
+  switch(choice)
+  {
+   case 1:
+   {
+    encryption();
+    break;
+   }
+   case 2:
+   {
+    decryption();
+    break;
+   }
+   case 3:
+    break;
+   default:
+   {
+    printf("Enter a valid choice\n");
+   }
+  }
+  if(choice==3)
+   return 0;
+ }
+}
+
+void encryption() {
+    int i, j, k;
+    getKeyMessage();
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 1; j++)
+            for(k = 0; k < 3; k++)
+                encrypt[i][j] = encrypt[i][j] + a[i][k] * mes[k][j];
+    
+    printf("\nEncrypted string is: ");
+    for(i = 0; i < 3; i++)
+        printf("%f", (double)(fmod(encrypt[i][0], 26) + 97));
+  printf("\n");
+}
+
+void decryption() {
+    int i, j, k;
+    getKeyMessage();
+    inverse();
+    
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 1; j++)
+            for(k = 0; k < 3; k++)
+                decrypt[i][j] = decrypt[i][j] + b[i][k] * encrypt[k][j];
+    
+    printf("\nDecrypted string is: ");
+    for(i = 0; i < 3; i++)
+        printf("%f", (double)(fmod(decrypt[i][0], 26) + 97));
+    
+    printf("\n");
+}
+
+void getKeyMessage() {
+    int i, j;
+    char msg[3];
+
+    printf("Enter 3x3 matrix for key (It should be inversible):\n");
+    
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++) {
+            scanf("%f", &a[i][j]);
+            c[i][j] = a[i][j];
+        }
+    
+    printf("\nEnter a 3 letter string: ");
+    scanf("%s", msg);
+    
+    for(i = 0; i < 3; i++)
+        mes[i][0] = msg[i] - 97;
+}
+
+void inverse() {
+    int i, j, k;
+    float p, q;
+    
+    for(i = 0; i < 3; i++)
+    {
+ printf("\n");
+        for(j = 0; j < 3; j++) {
+            if(i == j)
+                b[i][j]=1;
+            else
+                b[i][j]=0;
+            
+   printf("\t %f",b[i][j]);
+        }
+ }
+    for(k = 0; k < 3; k++) {
+        for(i = 0; i < 3; i++) {
+            p = c[i][k];
+            q = c[k][k];
+                
+            for(j = 0; j < 3; j++) {
+                if(i != k) {
+                    c[i][j] = c[i][j]*q - p*c[k][j];
+                    b[i][j] = b[i][j]*q - p*b[k][j];
+                }
+            }
+        }
+    }
+    
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            b[i][j] = b[i][j] / c[i][i];
+    
+    printf("\n\nInverse Matrix is:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++)
+            printf("%f ", b[i][j]);
+        
+        printf("\n");
+    }
+}
